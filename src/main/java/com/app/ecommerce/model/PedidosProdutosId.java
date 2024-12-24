f90d18b5dfcd7a9ed2acf03d7a93f5e5be8092b8
Gustav0Felipe <gustavo.custodio55@hotmail.com>
2024-12-24 15:12:48 -0300

    Entidade Pedido Implementada e Repository, Service e Controller Criados.

---
+++ src/main/java/com/app/ecommerce/model/PedidosProdutosId.java
@@ -0,0 +1,51 @@
+package com.app.ecommerce.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.ManyToOne;
+
+@Embeddable
+public class PedidosProdutosId implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@ManyToOne
+	private Produto id_prod;
+	
+	@ManyToOne
+    private Pedido pedido;
+
+	public Produto getId_prod() {
+		return id_prod;
+	}
+
+	public void setId_prod(Produto id_prod) {
+		this.id_prod = id_prod;
+	}
+
+	public Pedido getNum_ped() {
+		return pedido;
+	}
+
+	public void setNum_ped(Pedido pedido) {
+		this.pedido = pedido;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id_prod, pedido);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		PedidosProdutosId other = (PedidosProdutosId) obj;
+		return Objects.equals(id_prod, other.id_prod) && Objects.equals(pedido, other.pedido);
+	}
+}
---
+++ src/main/java/com/app/ecommerce/service/PedidoService.java
@@ -0,0 +1,8 @@
+package com.app.ecommerce.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PedidoService {
+
+}
