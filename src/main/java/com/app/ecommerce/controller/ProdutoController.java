514b31d107900fb88f07a1bd51d566cd680a859e
Gustav0Felipe <gustavo.custodio55@hotmail.com>
2025-03-10 11:25:54 -0300

    Ajuste Endpoint criarProduto

--- src/main/java/com/app/ecommerce/controller/ProdutoController.java
+++ src/main/java/com/app/ecommerce/controller/ProdutoController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -37,7 +38,7 @@ public class ProdutoController {
 	}
 	
 	@PostMapping
-	public ResponseEntity<Produto> criarProduto(@Valid Produto produto){
+	public ResponseEntity<Produto> criarProduto(@RequestBody @Valid Produto produto){
 		return ResponseEntity.ok(produtoService.criarProduto(produto));
 	}
 }
